79b84c9044388e1dbd5f9ceba75efd47c67a9aa2
Sahab Alfahad <sahabalfahad6@gmail.com>
2025-09-24 15:53:11 +0300

    Update customer.py

--- Bank/customer.py
+++ Bank/customer.py
@@ -9,4 +9,7 @@ class Customer:
         self.last_name = last_name
         self.password = password
         self.account = account 
-    
+    def full_name(self):
+          return f"{self.first_name}{self.last_name}".strip()
+        def verify_password(self,candidata_password):
+            return self.password == candidate_password
