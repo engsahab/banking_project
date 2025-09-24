65957554d5bfe5bbf5492b830a983d9f9722cd1a
Sahab Alfahad <sahabalfahad6@gmail.com>
2025-09-25 00:38:41 +0300

    Update test_customer.py
    Add unit tests to validate initialization, password checks, and name formatting.

--- tests/test_customer.py
+++ tests/test_customer.py
@@ -1,24 +1,34 @@
 import unittest
-from Bank.customer import Customer
-from Bank.account import Account
+from Bank.customer import Customer 
 
 class TestCustomer(unittest.TestCase):
-    def test_create_customer(self):
-        account = Account(account_id=1001)
-        customer = Customer(customer_id=1, first_name="Sahab", last_name="Alharbi", password="s@123" ,account=account)
-        self.assertEqual(customer.customer_id, 1)
-        self.assertEqual(customer.first_name, "Sahab")
-        self.assertEqual(customer.last_name, "Alharbi")
-        self.assertEqual(customer.password, "s@123")
-        self.assertIsNotNone(customer.account)
 
-    def test_full_name_and_password(self):
-        account = Account(account_id=2002)
-        customer = Customer(customer_id=2, first_name="Noura", last_name="Abdullah", password="p@ss", account=account)
-        self.assertEqual(customer.full_name(), "Noura Abdullah")
-        self.assertTrue(customer.verify_password("p@ss"))
-        self.assertFalse(customer.verify_password("wrong"))
+    def setUp(self):
+        
+        self.customer = Customer(
+            account_id='10001',
+            first_name='Sahab',
+            last_name='Alharbi',
+            password='S123'
+        )
 
-if __name__ =="__main__":
+    def test_customer_initialization(self):
+
+        self.assertEqual(self.customer.account_id, '10001')
+        self.assertEqual(self.customer.first_name, 'Sahab')
+        self.assertEqual(self.customer.last_name, 'Alharbi')
+        self.assertEqual(self.customer.password, 'S123')
+
+    def test_full_name(self):
+        self.assertEqual(self.customer.full_name(), 'Sahab Alharbi')
+
+    def test_verify_password_correct(self):
+        
+        self.assertTrue(self.customer.verify_password('S123'))
+
+    def test_verify_password_incorrect(self):
+        self.assertFalse(self.customer.verify_password('wrongpassword'))
+
+
+if __name__ == '__main__':
     unittest.main()
-
