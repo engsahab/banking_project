b4c209bf146d3c6d04897c74bcdaab7f232291fe
Sahab Alfahad <sahabalfahad6@gmail.com>
2025-09-25 00:29:53 +0300

    Update customer.py
    - feat: Add check_password ( ) method to verify user credentials for authentication.
    - feat: Add get_full_name ( ) method to format the customer's name for display.

--- Bank/customer.py
+++ Bank/customer.py
@@ -2,14 +2,14 @@
 from Bank.account import Account
 
 class Customer:
-    def __init__(self, customer_id, first_name, last_name, password, account=None):
-
-        self.customer_id = customer_id
+    def __init__(self, account_id, first_name, last_name, password):
+        self.account_id = account_id
         self.first_name = first_name
         self.last_name = last_name
         self.password = password
-        self.account = account 
+    
     def full_name(self):
-          return f"{self.first_name}{self.last_name}".strip()
-        def verify_password(self,candidata_password):
-            return self.password == candidate_password
+         return f"{self.first_name} {self.last_name}"
+    
+    def verify_password(self,password_to_check):
+            return self.password == password_to_check
