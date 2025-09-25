891cd08ded01c150325767d50f0c48192fff3e3e
Sahab Alfahad <sahabalfahad6@gmail.com>
2025-09-25 09:57:34 +0300

    Update csv_bank.py

--- csv_bank.py
+++ csv_bank.py
@@ -1,1 +1,13 @@
+import csv
+from Bank.customer import Customer
+from Bank.account import Account
 
+class Bank:
+    def __init__(self, filepath='bank.csv'):
+        self.filepath = filepath
+        self.customers = {}  
+        self.accounts = {}  
+        self.load_data()
+
+    def load_data(self):
+    
